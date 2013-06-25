ae28213fa7fae64affb5f440c5327db86e1e4167
rpq <rpq@winscores.com>
2013-06-25 12:03:59 -0400

    created ampm; tests for time minus field test and split

--- time_forms.py
+++ time_forms.py
@@ -1,3 +1,4 @@
+import datetime
 import math
 import time
 
@@ -5,6 +6,25 @@ from django.forms import widgets as django_widgets
 from django import forms as django_forms
 
 TIME_FORMAT = "%I:%M %p"
+
+def get_ampm(value):
+    def int_ampm(int_value):
+        try:
+            int_value = int(int_value)
+        except Exception:
+            return None
+
+        if int_value >= 12 and int_value < 24:
+            return 'PM'
+        else:
+            return 'AM'
+
+    if isinstance(value, (datetime.datetime, datetime.time,)):
+        return value.strftime('%p')
+    elif isinstance(value, (unicode, str, int,)):
+        return int_ampm(value)
+    else:
+        return None
 
 def round_to_five_minutes(actual_minute):
     return int(math.ceil(int(actual_minute)/5)*5)
@@ -67,9 +87,9 @@ class SplitTimeSelectWidget(django_widgets.MultiWidget):
             return [None, None, None]
 
         if value:
-            hour = value.tm_hour
-            min = value.tm_min
-            am_or_pm = 'am' if hour < 12 else 'pm'
+            hour = value.hour
+            min = value.minute
+            am_or_pm = get_ampm(hour).lower()
 
             return [hour, round_to_five_minutes(min), am_or_pm,]
 
@@ -104,3 +124,135 @@ class SplitTimeField(django_forms.MultiValueField):
                 hour, min, am_or_pm),
                 TIME_FORMAT)
             return s
+
+if __name__ == '__main__':
+    import os, sys
+    sys.path.append(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        'tests',
+        'django_more_forms_tests',))
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'django_more_forms_tests.settings'
+
+    import unittest
+    from django.test import SimpleTestCase
+
+    class TestGetAmPm(SimpleTestCase):
+        def test_get_with_int(self):
+            self.assertEqual(get_ampm(1), 'AM')
+            self.assertEqual(get_ampm(5), 'AM')
+            self.assertEqual(get_ampm(12), 'PM')
+            self.assertEqual(get_ampm(20), 'PM')
+
+        def test_get_with_time(self):
+            self.assertEqual(get_ampm(datetime.time(hour=1)), 'AM')
+            self.assertEqual(get_ampm(datetime.time(hour=5)), 'AM')
+            self.assertEqual(get_ampm(datetime.time(hour=12)), 'PM')
+            self.assertEqual(get_ampm(datetime.time(hour=20)), 'PM')
+
+        def test_get_with_datetime(self):
+            def create_dt(hour):
+                return datetime.datetime(year=2000, month=1, day=1,
+                    hour=hour)
+            self.assertEqual(get_ampm(create_dt(1)), 'AM')
+            self.assertEqual(get_ampm(create_dt(5)), 'AM')
+            self.assertEqual(get_ampm(create_dt(12)), 'PM')
+            self.assertEqual(get_ampm(create_dt(20)), 'PM')
+
+        def test_get_with_string(self):
+            self.assertEqual(get_ampm('1'), 'AM')
+            self.assertEqual(get_ampm('5'), 'AM')
+            self.assertEqual(get_ampm('12'), 'PM')
+            self.assertEqual(get_ampm('20'), 'PM')
+
+    class TimeOptionChoicesTest(SimpleTestCase):
+
+        def setUp(self):
+            self.hours = TimeOptionChoices.hours()
+            self.minutes = TimeOptionChoices.minutes()
+            self.ampm = TimeOptionChoices.ampm()
+
+        def test_hours_choices_exist(self):
+            # plus one for blank
+            self.assertEqual(len(self.hours), 12 + 1)
+
+        def test_minutes_choices_exist(self):
+            self.assertEqual(len(self.minutes), 60/5 + 1)
+
+        def test_ampm_choices_exist(self):
+            self.assertEqual(len(self.ampm), 2 + 1)
+
+        def test_blanks(self):
+            self.assertIn('---', map(lambda x: x[1], self.hours))
+            self.assertIn('---', map(lambda x: x[1], self.minutes))
+            self.assertIn('---', map(lambda x: x[1], self.ampm))
+
+    class TimeSelectWidgets(SimpleTestCase):
+
+        def setUp(self):
+            w = HourSelectWidget()
+            self.rendered_hours = w.render('hours', '')
+            w = MinuteSelectWidget()
+            self.rendered_minutes = w.render('minutes', '')
+            w = AmPmSelectWidget()
+            self.rendered_ampm = w.render('ampm', '')
+
+        def test_hour_widget(self):
+            w_rendered_expect = '<select class="hours-select" name="hours">'
+            self.assertIn(w_rendered_expect, self.rendered_hours)
+
+            self.assertIn('<option ', self.rendered_hours)
+            for value, display_value in TimeOptionChoices.hours():
+                self.assertIn(unicode(value), self.rendered_hours)
+                self.assertIn(unicode(display_value), self.rendered_hours)
+
+        def test_minute_widget(self):
+            w_rendered_expect = \
+                '<select class="minutes-select" name="minutes">'
+            self.assertIn(w_rendered_expect, self.rendered_minutes)
+
+            self.assertIn('<option ', self.rendered_minutes)
+            for value, display_value in TimeOptionChoices.minutes():
+                self.assertIn(unicode(value), self.rendered_minutes)
+                self.assertIn(unicode(display_value), self.rendered_minutes)
+
+        def test_ampm_widget(self):
+            w_rendered_expect = '<select class="ampm-select" name="ampm">'
+            self.assertIn(w_rendered_expect, self.rendered_ampm)
+
+            self.assertIn('<option ', self.rendered_ampm)
+            for value, display_value in TimeOptionChoices.ampm():
+                self.assertIn(unicode(value), self.rendered_ampm)
+                self.assertIn(unicode(display_value), self.rendered_ampm)
+
+    class SplitTimeSelectWidgetTest(SimpleTestCase):
+
+        def setUp(self):
+            self.w = SplitTimeSelectWidget()
+            self.rendered = self.w.render('time-select', '')
+
+        def test_widget_rendering_all_widgets(self):
+            # tests render
+            self.assertIn('<select class="hours-select" name="time-select_0"', self.rendered)
+            self.assertIn('<select class="minutes-select" name="time-select_1', self.rendered)
+            self.assertIn('<select class="ampm-select" name="time-select_2', self.rendered)
+
+        def test_widget_with_value(self):
+            now = datetime.datetime.now().time()
+            hour = now.hour
+            minute = now.minute
+            ampm = get_ampm(now).lower()
+
+            # tests decompress
+            self.rendered = self.w.render('time-select', now)
+            hour_assert = '<option value="{0}" selected="selected">'.format(hour)
+            minute_assert = '<option value="{0}" selected="selected">'.format(
+                round_to_five_minutes(minute))
+            ampm_assert = '<option value="{0}" selected="selected">'.format(
+                ampm)
+
+            self.assertIn(hour_assert, self.rendered)
+            self.assertIn(minute_assert, self.rendered)
+            self.assertIn(ampm_assert, self.rendered)
+
+
+    unittest.main()
